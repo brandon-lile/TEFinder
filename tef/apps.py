b4e7154aa055aa981d96b81026ab87e26132fb60
Brandon Lile <brandon.lile@gmail.com>
2016-02-28 21:23:16 -0600

    Adding tef app and correcting urls.py

---
+++ tef/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TefConfig(AppConfig):
+    name = 'tef'
--- tefinder/settings.py
+++ tefinder/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tefinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tefinder',
+        'USER': 'tefinder',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
