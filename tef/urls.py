dfc9ef55e1dddde2986f7edf15b58b07b76d581a
Brandon Lile <brandon.lile@gmail.com>
2016-03-01 01:13:24 -0600

    Changing url structure for ease of form use

--- tef/urls.py
+++ tef/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 app_name = 'tef'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^search/', views.search, name='search'),
+    url(r'^$', views.search, name='search'),
     url(r'^review/?P<te_id>[0-9]+/$', views.review, name='review')
 ]
--- tef/views.py
+++ tef/views.py
@@ -1,31 +1,36 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import render
+from .forms import SearchForm
 from .tasks import queue_te
 from .models import TE
 
 
 # Shows search page
-def index(request):
-    all_searches = TE.objects.order_by('query_date')[:5]
-    context = {'searches': all_searches}
-    return render(request, 'tef/index.html', context)
-
-
 def search(request):
-    # Create original object
-    t = TE(
-        user=request.user,
-        query=request.POST['te_query']
-    )
-    t.save()
 
-    # Throw into celery
-    queue_te(t)
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
 
-    # Send them to the results page
-    return HttpResponse("Search")
+        if form.is_valid():
+            # Create original object
+            t = TE(
+                user=request.user,
+                query=request.POST['te_query']
+            )
+            t.save()
+
+            # Throw into celery
+            queue_te(t)
+
+            return HttpResponseRedirect('tef:review', args=(t.id,))
+        else:
+            context = {'search_form': form}
+            return render(request, 'tef/index.html', context)
+    else:
+        context = {
+            'search_form': SearchForm()
+        }
+        return render(request, 'tef/index.html', context)
 
 
 def review(request, te_id):
