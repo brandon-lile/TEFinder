cacfda94b9a10f755d9f213dbb6682a0d21fdd28
Brandon Lile <brandon.lile@gmail.com>
2016-03-01 01:14:14 -0600

    Adding SearchForm class

--- tef/forms.py
+++ tef/forms.py
@@ -1,18 +1,13 @@
 from django import forms
-from django.forms import Textarea
-from django.core.validators import RegexValidator
-
-
-class DNAField(Textarea):
-    default_validators = [
-        RegexValidator(
-            '^[ACTGactg]*$',
-            'Must be a valid DNA sequence'
-        )
-    ]
 
 
 class SearchForm(forms.Form):
-    query = DNAField()
-    start_loc = forms.NumberInput(label='Start Location')
-    end_loc = forms.NumberInput(label='End Location')
+    query = forms.RegexField(
+        '^[ACTGactg]+$',
+        label='DNA Sequence'
+    )
+    start_loc = forms.IntegerField(
+        label='Start Location',
+        min_value=0
+    )
+    end_loc = forms.IntegerField(label='End Location')
