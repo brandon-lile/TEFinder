99f2e9a7f91956eda5e41a6e9bb5f1b0cc93855c
Brandon Lile <brandon.lile@gmail.com>
2016-02-29 19:49:10 -0600

    Adding TE model and migration

--- tef/models.py
+++ tef/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class TE(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    query = models.TextField()
+    query_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.query
