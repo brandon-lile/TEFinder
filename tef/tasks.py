18ddc950f08ae7547ba6b10766716f6446e8cf34
Brandon Lile <brandon.lile@gmail.com>
2016-03-01 21:37:18 -0600

    Removing checks for now. Finished algorithm

--- tef/tasks.py
+++ tef/tasks.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from celery import shared_task
+import json
 
 
 # Solve problem, put in dictionary, json, throw in database
@@ -17,19 +18,42 @@ def queue_te(te):
         te.start_loc = min(te.start_loc, len(te.query))
 
     # Check if we need to change end_loc
-    if te.end_loc > 0 and te.end_loc > te.start_loc:
-        te.end_loc = min(te.end_loc, len(te.query))
-    elif te.end_loc > 0 and te.end_loc < te.start_loc:
-        te.end_loc = te.start_loc
-    elif te.end_loc == 0:
-        te.end_loc = len(te.query)
+    #if te.end_loc > 0 and te.end_loc > te.start_loc:
+    #    te.end_loc = min(te.end_loc, len(te.query))
+    #elif 0 < te.end_loc < te.start_loc:
+    #    te.end_loc = te.start_loc
+    #elif te.end_loc == 0:
+    #    te.end_loc = len(te.query)
 
-    original_query = te.query
-    te.query = te.query[start_loc:end_loc] # May have to change this - indexes could be made wonky above
+    #original_query = te.query
+    # May have to change this - indexes could be made wonky above
+    #te.query = te.query[te.start_loc:te.end_loc]
 
     te.reverse_query = ''
     for x in te.query:
         te.reverse_query = dna_translate[x] + te.reverse_query
 
+    potential = {}
+    str_len = len(te.query) - 1
+    cur_pos = str_len
+    while cur_pos >= 0:
+        total = 0
+        in_sol = False
+        for x in range(cur_pos, str_len):
+            # Compare base by base
+            if te.query[x] == te.reverse_query[x - cur_pos]:
+                total += 1
+                if total > te.threshold and in_sol is False:
+                    in_sol = True
+                    potential[cur_pos] = [x for x in range(x - te.threshold, x)]
+                    potential[cur_pos].append(x)
+                elif total > te.threshold and in_sol is not False:
+                    potential[cur_pos].append(x)
+            else:
+                total = 0
+
+        cur_pos -= 1
+
+    te.solution = json.dumps(potential)
     te.save()
     return None
--- tef/urls.py
+++ tef/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'tef'
 urlpatterns = [
     url(r'^$', views.search, name='search'),
-    url(r'^review/?P<te_id>[0-9]+/$', views.review, name='review')
+    url(r'^review/(?P<te_id>[0-9]+)/$', views.review, name='review')
 ]
